b79233ef7d0952f44cd2a2f652e6ab64e597fd10
Woecifaun <woecifaun@gmail.com>
2014-05-01 16:10:01 +0200

    add SVG->PNG generator banners PHP Tour Lyon

---
+++ PHP Tour/2014 Lyon/banners/web/generator.php
@@ -0,0 +1,42 @@
+<?php
+// file path control
+if (!isset($_GET['badge']) || !preg_match('/^[a-z]+$/', $_GET['badge'])) {
+    header('HTTP/1.0 400 Bad Request', true, 400);
+    die('invalid data : '.__LINE__);
+}
+
+// file content check
+$svg = file_get_contents('banners/base_728x90_'.$_GET['badge'].'_clean.svg');
+if (!$svg) {
+    header('HTTP/1.0 400 Bad Request', true, 400);
+    die('invalid data : '.__LINE__);
+}
+
+// colors check
+$colorNames = array('primaryColor', 'secondaryColor', 'ternaryColor');
+foreach ($colorNames as $colorName) {
+    if (!isset($_GET[$colorName]) || !preg_match('/^[0-9a-f]{6}$/', $_GET[$colorName])) {
+        header('HTTP/1.0 400 Bad Request', true, 400);
+        die('invalid data : '.__LINE__);
+    }
+    ${$colorName} = '#'.$_GET[$colorName];
+}
+
+// update colors
+// SVG paths are stylised through CSS, with three colors
+// SVG is loaded as a string here so we use a simple string funtion to change colors
+// color codes (#012345, #56789a, #abcdef) have been chosen to avoid collision with other data
+// but who knows
+$svg = str_replace('#012345', $primaryColor, $svg); 
+$svg = str_replace('#56789a', $secondaryColor, $svg); 
+$svg = str_replace('#abcdef', $ternaryColor, $svg); 
+
+$image = new Imagick();
+$image->readImageBlob($svg);
+$image->setImageFormat("png24");
+
+header('Content-type:image/png');
+echo $image->getImage();
+
+$image->clear();
+$image->destroy();
